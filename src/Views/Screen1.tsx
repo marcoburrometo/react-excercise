5372e771f7c710addae5c1029585c093ff6ce795
marcoburrometo <marco.burrometo@gmail.com>
2020-10-03 11:46:19 +0200

    :sparkles: Screen 2 modal

--- src/Views/Screen1.tsx
+++ src/Views/Screen1.tsx
@@ -1,6 +1,20 @@
-import React from "react";
+import { Button } from '@material-ui/core';
+import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 export default function Screen1() {
-
-  return <h1>Screen 1</h1>;
+  const history = useHistory();
+  return (
+    <>
+      <h1>Screen 1</h1>
+      <Button
+        variant="contained"
+        onClick={() => {
+          history.push('/screen2');
+        }}
+      >
+        Go to screen 2
+      </Button>
+    </>
+  );
 }
--- src/Views/Screen2.tsx
+++ src/Views/Screen2.tsx
@@ -1,5 +1,7 @@
-import { Button } from '@material-ui/core';
 import React from 'react';
+import {
+  Button, Dialog, DialogContent, DialogTitle,
+} from '@material-ui/core';
 import { useHistory } from 'react-router-dom';
 
 export default function Screen2() {
@@ -9,9 +11,14 @@ export default function Screen2() {
   };
 
   return (
-    <div>
-      <h1>Screen 2</h1>
-      <Button variant="contained" onClick={navigate}>Go to screen 1</Button>
-    </div>
+    <Dialog aria-labelledby="user-dialog-title" open scroll="paper" maxWidth="lg" fullWidth>
+      <DialogTitle id="user-dialog-title">Title</DialogTitle>
+      <DialogContent className="person-dialog" style={{ padding: 30 }} dividers={true}>
+        <h1>Screen 2</h1>
+        <Button variant="contained" onClick={navigate}>
+          Go to screen 1
+        </Button>
+      </DialogContent>
+    </Dialog>
   );
 }
